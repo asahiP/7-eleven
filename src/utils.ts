a43c7483ff98f7b6e4965c88cafaeccf4cc4429c
Luke Pan <asahi0749@gmail.com>
2020-05-26 22:48:58 +0800

    - 新增：公共组件<ScrollView> - 新增：utils中characterRange函数

--- src/utils.ts
+++ src/utils.ts
@@ -98,3 +98,15 @@ export function range (...args: number[]): number[] {
       })
   }
 }
+
+export function characterRange (start: string, end: string): string[] {
+  let [min, max] = [start ,end]
+    /** 转换Unicode */
+    .map(i => i.charCodeAt(0))
+    /** 重新排序 */
+    .sort((a, b) => a - b)
+
+  return range(min, max + 1)
+    /** 遍历数组并返回字符串 */
+    .map(i => String.fromCharCode(i))
+}
